1f86b59b06456ef7c525c768d38a6bea31d257b9
Dustin Evans <dustine@turnkeysol.com>
2019-11-06 18:52:02 -0600

    Working on challenge6

--- CryptoPals/Set1/challenge6/challenge6.py
+++ CryptoPals/Set1/challenge6/challenge6.py
@@ -1,6 +1,6 @@
 def b64DecodeFile(filename):
     from base64 import b64decode
-    fd = open(filename,'r')
+    fd = open(filename,'rb')
     b64 = fd.read()
     b64d = b64decode(b64)
     return b64d
@@ -70,7 +70,6 @@ def calculateIC(textbytes):
             result.append(temp)
         ic = 0
         for thing in result:
-            print(thing)
             count = thing['count']
             ic += count*(count-1.00)
         return ic/(textlength*(textlength-1.00))
@@ -78,35 +77,41 @@ def calculateIC(textbytes):
         return None
 
 def getBlocks(textbytes,keysize):
-    keysize = keysize+1
+    from math import floor
+    import numpy as np
     if type(textbytes) == str:
         textbytes = bytearray(textbytes,'utf8')
-    array1 = []
-    counter = 1
-    temparray = []
-    for char in textbytes:
-        if counter%keysize == 0 and counter != 0:
-            array1.append(temparray)
-            temparray = []
-        else:
-            temparray.append(char)
-        counter+=1
+    textbytes1 = np.frombuffer(textbytes,dtype='S1')
+    array1 = np.array_split(textbytes1,len(textbytes)//keysize)
+    # keysize = keysize
+    # array1 = []
+    # counter = 0
+    # temparray = []
+    # for char in textbytes:
+    #     if (counter%(keysize) == 0) and (counter != 0):
+    #         array1.append(temparray)
+    #         temparray = []
+    #     temparray.append(char)
+    #     counter+=1
     return array1
 
-def unzipBlocks(blocks,keysize):
-    unzipped = [[0]*keysize]*len(blocks)
-    for x in range(len(blocks)):
-        for y in range(keysize):
-            unzipped[y][x]=blocks[x][y]
-    return unzipped
+def transposeBlocks(blocks):
+    import numpy as np
+    transposed = np.transpose(blocks)
+    return transposed.tolist()
 
-file = b64DecodeFile('./6.txt')
-keysize = determineKeySize(file,2,40)
-print(keysize)
-print(calculateIC(""))
-blocks = getBlocks(file,keysize)
-for i in blocks:
-    print(i)
-unzipped = unzipBlocks(blocks,keysize)
-for i in unzipped:
-    print(i)
+data = b64DecodeFile('./6.txt')
+keysize = determineKeySize(data,2,40)
+print("Keysize: %s"%keysize)
+blocks = getBlocks(data,keysize)
+print("Total blocks: %s"%len(blocks))
+print("Expected blocks: %s"%(len(data)//keysize))
+print("Length of each block: %s"%len(blocks[0]))
+print("TextLength: %s"%len(data))
+print("Text length / keysize: %s"%(len(data)/keysize))
+transposed = transposeBlocks(blocks)
+for i in range(10):
+    print(blocks[i])
+print()
+for i in range(10):
+    print(transposed[i])
